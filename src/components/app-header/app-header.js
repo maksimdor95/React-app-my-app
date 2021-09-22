9df4b3b0af847f5e574c8e308297cb23bf717dde
Maxim <maximka5991@gmail.com>
2021-09-22 11:24:10 +0300

    liked

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -19,11 +19,11 @@ const Header = styled.div`
      }
 `
 
-const AppHeader = () => {
+const AppHeader = ({liked, allPosts}) => {
     return (
         <Header>
             <h1>Maxim Dorochov</h1>
-            <h2>5 записей, из них понравилось 0</h2>
+            <h2>{allPosts} записей, из них понравилось {liked}</h2>
         </Header>
     )
 }
