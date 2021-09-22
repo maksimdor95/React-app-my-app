67c552dae318eda00de161a068546599eacf3862
Maxim <maximka5991@gmail.com>
2021-09-22 10:24:42 +0300

    deliteItem

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -1,35 +1,40 @@
 import React from 'react';
 
 import PostListItem from '../post-list-item';
-import { ListGroup} from 'reactstrap';
+// import { ListGroup} from 'reactstrap';
 import './post-list.css';
 
-const PostList = ({posts}) => {
+const PostList = ({posts, onDelete}) => {
 
-    const elements = posts.map( (item) => {
+    const elements = posts.map((item) => {
+        const {id, ...itemProps} = item;
+        return (
+            <li key = {id} className='list-group-item'>
+                <PostListItem 
+                    {...itemProps}
+                    onDelete={() => onDelete(id)}/>
+            </li>
+        )
+
         // Простой способ проверки на объект + содержится ли в нем информация
-        if ( typeof item === 'object' && isEmpty(item) ){ 
-            const {id, ...itemProps} = item;
-            return (
-                <li key = {id} className='list-group-item'>
-                    <PostListItem {...itemProps}/>
-                </li>
-            )
-        }
-    })
+        // if ( typeof item === 'object' && isEmpty(item) ){ 
+            
+            
+        
+    });
 
-    function isEmpty(obj) {
-        for(let key in obj)
-        {
-            return true;
-        }
-        return false;
-    }
+    // function isEmpty(obj) {
+    //     for(let key in obj)
+    //     {
+    //         return true;
+    //     }
+    //     return false;
+    // }
 
     return (
-        <ListGroup className="app-list">
+        <ul className="app-list list-group">
            {elements}
-        </ListGroup>
+        </ul>
     )
 }
 
